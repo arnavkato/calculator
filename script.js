ceba01dac60e0322f1253984dfae07d99b13aa18
arnavkato <arnavkatoch47@gmail.com>
2025-02-27 16:11:36 -0500

    add html/css structure

--- script.js
+++ script.js
@@ -14,3 +14,44 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+": add(num1, num2);
+            break;
+        case "-": subtract(num1, num2);
+            break;
+        case "*": multiply(num1, num2);
+            break;
+        case "/": divide(num1, num2);
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let num1;
+let num2;
+let operator;
